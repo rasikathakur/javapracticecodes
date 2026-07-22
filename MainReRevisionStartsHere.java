17f563e3c627f37e1b095b66c8e3814ab36e1bc1
Rasika Thakur <125433090+rasikathakur@users.noreply.github.com>
2026-07-22 23:15:31 +0530

    Add multiple programs for number operations and patterns

--- MainReRevisionStartsHere.java
+++ MainReRevisionStartsHere.java
@@ -107,30 +107,183 @@ class Main {
                     break;
                 case 6:
                     // Create a program to find the Greatest Common Divisor (GCD) of two integers.
+                    System.out.println("Enter number 1:");
+                    int num3 = sc.nextInt();
+                    System.out.println("Enter number 2:");
+                    int num4 = sc.nextInt();
+                    int x = num3;
+                    int y = num4;
+                    // method 1
+                    while(y!=0){
+                        int remainder = x % y;
+                        x = y;
+                        y = remainder;
+                    }
+                    System.out.println("The GCD is: " + x);
+                    // method 2
+                    while(x != y){
+                        if(x > y){
+                            x = x - y;
+                        }else{
+                            y = y - x;
+                        }
+                    }
+                    System.out.println("The GCD is: " + x);
                     break;
                 case 7:
                     // Create a program to check whether a given number is prime.
+                    System.out.println("Enter number to check for prime:");
+                    int num5 = sc.nextInt();
+                    boolean isPrime = true;
+                    if(num5 <= 1){
+                        isPrime = false;
+                    }else{
+                        i = 2; 
+                        while(i < num5){
+                            if(num5 % i == 0){
+                                isPrime = false;
+                                break;
+                            }
+                            i++;
+                        }
+                    }
+                    
+                    if(isPrime){
+                        System.out.println("Number is a prime number");
+                    }else{
+                        System.out.println("Number is not a prime number");
+                    }
+                    
                     break;
                 case 8:
                     // Create a program to reverse the digits of a number.
+                    System.out.println("Enter number to reverse:");
+                    int num6 = sc.nextInt();
+                    temp = num6;
+                    int reverse = 0;
+                    
+                    while(temp > 0){
+                        int lastdigit = temp % 10;
+                        reverse = (reverse*10)+lastdigit;
+                        temp = temp / 10;
+                    }
+                    System.out.println("Original Number "+num6);
+                    System.out.println("Reversed Number "+reverse);
                     break;
                 case 9:
                     // Create a program to print the Fibonacci series up to a certain number
+                    System.out.println("Enter number to generate fibonnaci series:");
+                    int num7 = sc.nextInt();
+                    int firstterm = 0;
+                    int secondterm = 1;
+                    i = 1;
+                    while(i <= num7){
+                        System.out.print(firstterm+", ");
+                        int nextterm = firstterm + secondterm;
+                        firstterm = secondterm;
+                        secondterm = nextterm;
+                        i++;
+                    }
+                    System.out.println();
                     break;
                 case 10:
                     // Create a program to check if a number is an Armstrong number.
+                    System.out.println("Enter number to check for armstrong:");
+                    int num8 = sc.nextInt();
+                    int count = (Integer.toString(num8)).length();
+                    temp = num8;
+                    sum = 0;
+                    while(temp > 0){
+                        int lastd = temp % 10;
+                        sum += Math.pow(lastd,count);
+                        temp = temp/10;
+                    }
+                    if(sum == num8){
+                        System.out.println(num8+ " is Armstrong Number ");
+                    }else{
+                        System.out.println(num8+ " is not an Armstrong Number ");
+                    }
                     break;
                 case 11:
                     // Create a program to verify if a number is a palindrome.
+                    System.out.println("Enter number to check for palindrome:");
+                    int num9 = sc.nextInt();
+                    temp = num9;
+                    reverse = 0;
+                    while(temp > 0){
+                        int ldigit = temp % 10;
+                        reverse = (reverse*10)+ldigit;
+                        temp = temp/10;
+                    }
+                    if(reverse == num9){
+                        System.out.println(num9+ " is palindrome Number ");
+                    }else{
+                        System.out.println(num9+ " is not an palindrome Number ");
+                    }
                     break;
                 case 12:
                     // Create a program that print pattern 1
+                    /*
+                        *
+                        * *
+                        * * *
+                        * * * *
+                        * * * * *
+                    */
+                    i = 0;
+                    while (i <= 5){
+                        j = 0;
+                        while(j <= i){
+                            System.out.print("* ");
+                            j++;
+                        }
+                        System.out.println();
+                        i++;
+                    }
                     break;
                 case 13:
                     // Create a program that print pattern 2
+                    /*
+                        * * * * *
+                        * * * *
+                        * * *
+                        * *
+                        *
+                    */
+                    i = 5;
+                    while (i >= 0){
+                        j = 0;
+                        while(j <= i){
+                            System.out.print("* ");
+                            j++;
+                        }
+                        System.out.println();
+                        i--;
+                    }
                     break;
                 case 14:
                     // Create a program that print pattern 3
+                    /*
+                                *
+                              * *
+                            * * *
+                          * * * *
+                        * * * * *
+                    */
+                    i = 5;
+                    while (i >= 1){
+                        j = 1;
+                        while(j < i){
+                            System.out.print(" ");
+                            j++;
+                        }
+                        while(j < 5){
+                            System.out.print("*");
+                            j++;
+                        }
+                        System.out.println();
+                        i--;
+                    }
                     break;
             }
         }while(ch <=14);
