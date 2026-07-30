09f41c102214ab29e66649db7e502b85e1163b0c
Rasika Thakur <125433090+rasikathakur@users.noreply.github.com>
2026-07-30 17:16:15 +0530

    Create RunningSumOfArray.java

---
+++ RunningSumOfArray.java
@@ -0,0 +1,10 @@
+class RunningSumOfArray {
+    public int[] runningSum(int[] nums) {
+        int i = 1;
+        while(i < nums.length){
+            nums[i] = nums[i]+nums[i-1];
+            i++;
+        }
+        return nums;
+    }
+}
